4d0cda7080040559ef54a447aa6dc956ce03ed37
Bradley Bynum <bynumb1@gmail.com>
2016-01-06 20:52:50 -0500

    Broken translate

---
+++ stringops/stringops.js
@@ -0,0 +1,17 @@
+'use strict';
+
+exports.reverse = function(reversal) {
+    console.log(reversal);
+    var len=reversal.length-1;
+    var ret="";
+    for(;len >=0; len--) {
+        ret+=reversal[len];
+    }
+    return ret;
+}
+
+exports.piglatin = function(translate) {
+    
+}
+
+
