f79d2c9fde28e797131e809461e0cc847efe8f61
Bradley Bynum <bynumb1@gmail.com>
2016-01-06 21:24:46 -0500

    Working unit tests

--- stringops/src/stringops.js
+++ stringops/src/stringops.js
@@ -10,4 +10,22 @@ exports.reverse = function(reversal) {
     return ret;
 }
 
-
+exports.piglatin = function (translate) {
+        var len=translate.length;
+        var sub;
+        var last;
+        if(translate[0]==='a' 
+                || translate[0]==='e'
+                || translate[0]==='i'
+                || translate[0]==='o'
+                || translate[0]==='u') {
+                    sub=translate+'ay';
+                    console.log(sub)
+                    return sub;
+        } else {
+           sub=translate.substring(1,len)+translate.substring(0,1)
+           console.log(sub)
+           last = exports.piglatin(sub);
+           return last;
+        }
+}
--- stringops/test/string_ops.js
+++ stringops/test/string_ops.js
@@ -1,8 +1,15 @@
 var should = require('should');
-var ops = require('../stringops.js');
+var ops = require('../src/stringops.js');
 describe("stringops", function() {
     it("Should reverse a string", function() {
         var rev = ops.reverse("test");
         rev.should.equal("tset");
+    });
+});
+
+describe("PigLatin", function() {
+    it("Should verify piglatin translation", function() {
+        var trans=ops.piglatin('thrust');
+        trans.should.equal("ustthray");
     });
 });
